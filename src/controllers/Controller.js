5f641cebacf3d25f653d197d04e5f2cdaf562a9b
ValdemirPraxedes <valdemirpraxedes92@gmail.com>
2025-01-31 18:51:16 -0300

    adicionado controler pessoa

---
+++ src/controllers/Controller.js
@@ -0,0 +1,32 @@
+class Controller {
+    constructor(entidadeService) {
+        this.entidadeService = entidadeService;
+    }
+
+    async pegaTodos(req, res) {
+        try {
+            const listaDeRegistros = await this.entidadeService.pegaTodosOsRegistros();
+            return res.status(200).json(listaDeRegistros);
+        } catch (erro) {
+            // erro
+        }
+    }
+
+    async atualiza(req, res) {
+        const { id } = req.params;
+        const dadosAtualizados = req.body;
+        try {
+            const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number(id));
+
+            if(!foiAtualizado) {
+                return res.status(400).json({mensagem: `registro não atualizado`});
+            }
+
+            return res.status(200).json({mensagem: `registro atualizado`});
+
+        } catch (error) {
+            // erro
+        }
+    }
+}
+module.exports = Controller;
---
+++ src/services/Services.js
@@ -0,0 +1,24 @@
+const dataSource = require('../models');
+
+
+class Services {
+    constructor(nomeDoModel) {
+        this.model = nomeDoModel;
+    }
+
+    async pegaTodosOsRegistros() {
+        return dataSource[this.model].findAll();
+    }
+
+    async atualizaRegistro(dadosAtualizados, id) {
+        const listaDeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, { where:{id}});
+
+        if(listaDeRegistrosAtualizados[0] == 0) {
+            return false;
+        } 
+
+        return true;
+    }
+}
+
+module.exports = Services;
